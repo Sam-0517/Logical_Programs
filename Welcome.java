039dd35126127b92341429d7b83fe7c6821343d8
Sam-0517 <samadhankorde2000@gmail.com>
2022-12-26 22:27:17 +0530

    Added : Welcome message

---
+++ Welcome.java
@@ -0,0 +1,11 @@
+package Com.BridgeLabz.LogicalProgram;
+
+public class Welcome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Logical Programs ");
+
+	}
+
+}
