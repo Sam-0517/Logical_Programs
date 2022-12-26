118ba855fbbba5b6853e996432847eab6b6a87ca
Sam-0517 <samadhankorde2000@gmail.com>
2022-12-26 22:40:28 +0530

    Added : Code for Fibonacci Series

---
+++ FibonacciSeries.java
@@ -0,0 +1,25 @@
+package Com.BridgeLabz.LogicalProgram;
+
+import java.util.Scanner;
+
+public class FibonacciSeries {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1 = 0;
+		int num2 = 1;
+		int num3;
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter length to print series ");
+		int length = scan.nextInt();
+		System.out.println("First " + length + " number fibonacci series is");
+		System.out.print(num1 + " " + num2);
+
+		for (int i = 2; i < length; i++) {
+			num3 = num1 + num2;
+			num1 = num2;
+			num2 = num3;
+			System.out.print(" " + num3);
+		}
+	}
+}
