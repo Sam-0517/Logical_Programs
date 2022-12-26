c059e7991b0442870477feda0c32099c1989bae6
Sam-0517 <samadhankorde2000@gmail.com>
2022-12-26 22:43:24 +0530

    Added : Code for checking prime number

---
+++ PrimeNumber.java
@@ -0,0 +1,25 @@
+package Com.BridgeLabz.LogicalProgram;
+
+import java.util.Scanner;
+
+public class PrimeNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num;
+		int temp = 0;
+		Scanner scan = new Scanner(System.in);
+		num = scan.nextInt();
+		for (int i = 2; i < num; i++) {
+			if (num % i == 0) {
+				temp++;
+				break;
+			}
+		}
+		if (temp == 0) {
+			System.out.println("Number is prime ");
+		} else
+			System.out.println("Number is not prime ");
+	}
+
+}
