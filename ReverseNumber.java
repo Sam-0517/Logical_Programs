e1f46a4fe534b3353cdbb431c844823ea6a25338
Sam-0517 <samadhankorde2000@gmail.com>
2022-12-28 21:51:16 +0530

    Added: code for getting reverse number

--- ReverseNumber.java
+++ ReverseNumber.java
@@ -12,13 +12,13 @@ public class ReverseNumber {
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Enter number to print reverse ");
 		num = scan.nextInt();
-		System.out.println("Given number is "+num);
+		System.out.println("Given number is " + num);
 		while (num != 0) {
 			rem = num % 10;
 			reverse = (reverse * 10) + rem;
 			num = num / 10;
 		}
-		System.out.println("Reverse of number is "+reverse);
+		System.out.println("Reverse of number is " + reverse);
 	}
 
 }
