d5c8b466318b9e622e500bf33f94093292b633cf
Sam-0517 <samadhankorde2000@gmail.com>
2022-12-28 14:40:52 +0530

    Added : Utility code for Scanner Class

---
+++ Utility.java
@@ -0,0 +1,13 @@
+package Com.BridgeLabz.LogicalProgram;
+
+import java.util.Scanner;
+
+public class Utility {
+
+	private static Scanner scan = new Scanner(System.in);
+	
+	public static int getIntValue(){
+		
+		return scan.nextInt();
+	}
+}
